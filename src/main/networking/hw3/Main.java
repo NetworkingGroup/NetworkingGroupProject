235997f13e4c2446a7267ce13fe94de569bb7d26
nedingto <nedingto@oswego.edu>
2016-04-26 17:19:11 -0400

    Added skeleton main class, with basic initialization loop

--- src/main/networking/hw3/Main.java
+++ src/main/networking/hw3/Main.java
@@ -1,8 +1,30 @@
 package networking.hw3;
 
 
+import java.util.ArrayList;
+
 public class Main {
+    static final int CHUNK_SIZE = 12;//random size for each chunk
     public static void main(String[] args) {
-        System.out.println("Main");
+        //each value result will go in here, -1 means the chunk is being worked on, -2 means it is free to be worked on
+        ArrayList<Integer> log = new ArrayList<>();
+
+        String baseUrl = "someUrl"; //user provided url for getting data
+        int maxIndex =  20; //user provided limit to the amount of data they want
+        for(int i = 0; i < maxIndex; i++){
+            log.add(-2);//initilize every index to "ready to be worked on"
+        }
+        Anylitics a = new Anylitics(baseUrl);
+        //only stop if all the pending chunks are gone, all of the ready chunks are done
+        while (log.contains(-1) && log.contains(-2)) {
+            int toWorkOn = -1;
+            toWorkOn = log.indexOf(0);
+            a.anylize(toWorkOn*CHUNK_SIZE, (1+toWorkOn)*CHUNK_SIZE); //analyze chunk, run in background if possible
+            //send proposition
+            //check for join requests
+            //check for response to propositions
+            //check of other propositions
+            //check timers for indexes with -1, if it's timer is up change value to zero
+        }
     }
 }
